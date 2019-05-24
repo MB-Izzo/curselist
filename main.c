24240f8421c5df9c2cb97510a25503369cf7962b
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2019-05-25 00:03:33 +0200

    Testing out curses functions

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include "ncurses.h"
+#include "string.h"
 
 int main() 
 {
     initscr();
-    printw("Hello world!");
+    echo();
+    // echo() noecho()
+    // raw() cbreak()
+    // keypad()
+    char buf[80];
+    char bla[] = "Salut";
+    int row, col;
+    getmaxyx(stdscr, row, col);
+    mvprintw(4, (col-strlen(bla)) / 2, "%s", bla);
+    attron(A_REVERSE);
+    mvprintw(row/2, 5, "Input: ");
+    attroff(A_REVERSE);
+    move(row/2, 12);
+    do {
+        getstr(buf);
+        mvprintw(6, 20, "You entered: %s", buf);
+    } while (strcmp(buf, "STOP"));
     refresh();
-    getch();
     endwin();
     return 0;    
 }
